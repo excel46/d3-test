450fa1b968710785ba71507c7947ab23fedce6d3
Narain Mittal <mittalnarain123@gmail.com>
2016-11-10 14:05:26 -0800

    SVG bars render

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,36 @@
 'use strict';
-var data =[50, 11, 150, 199, 500, -10];
+var data =[50, 11, 150, 199, 500];
 var chart={w: 600, h: 600};
+var barwidth=30;
  
 var scale= d3.scaleLinear()
 			.domain([0,d3.max(data)])
 			.range([0,chart.h]);
-var chart= d3.select(".chart")
-				.style("width", chart.w+'px')
-				.style("height", chart.h+'px')
-				.selectAll("div")
-			.data(data)
-				.enter()
-				.append("div")
-					.classed("bar",true)
-					.text((d)=> d)
-					.style('height', (d) => (d>0?scale(d):0)+'px')
-					.style('margin-top',d=> (d>0?chart.h-scale(d):chart.h)+'px')
-				.exit().remove()
+var update= d3.select(".chart")
+				.append("svg")
+				.attr("class", "svg")
+				.attr("width", chart.w)
+				.attr("height", chart.h)
+				.selectAll("g")
+					.data(data);
+				
+var bars= update.enter()
+					.append("g")
+						.attr("transform", (d,i) =>  "translate("+(i*barwidth + i*5 )+","+(chart.h - scale(d) +1 )+")");
+						
+bars.append("rect")
+	.attr("class", "rect")
+	.attr("width", barwidth)
+	
+	.attr("height", (d)=> scale(d));
+
+	bars.append("text")
+    .attr("x", barwidth / 5  )
+    .attr("y", (d)=> scale(d) -3 )
+    .text(function(d) { return d; });
+					
+update.exit().remove();
+					
 						
 						
 						
