804f680724459c09f8637f32d2acb96dac167cb4
Narain Mittal <mittalnarain123@gmail.com>
2016-11-10 12:08:42 -0800

    Vertical bars

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,10 @@
-var data =[50, 11, 150, 199, 1000, -10];
+'use strict';
+var data =[50, 11, 150, 199, 500, -10];
 var chart={w: 600, h: 600};
  
 var scale= d3.scaleLinear()
 			.domain([0,d3.max(data)])
-			.range([0,chart.w]);
+			.range([0,chart.h]);
 var chart= d3.select(".chart")
 				.style("width", chart.w+'px')
 				.style("height", chart.h+'px')
@@ -13,7 +14,8 @@ var chart= d3.select(".chart")
 				.append("div")
 					.classed("bar",true)
 					.text((d)=> d)
-					.style('width', (d) => scale(d)+'px')
+					.style('height', (d) => (d>0?scale(d):0)+'px')
+					.style('margin-top',d=> (d>0?chart.h-scale(d):chart.h)+'px')
 				.exit().remove()
 						
 						
